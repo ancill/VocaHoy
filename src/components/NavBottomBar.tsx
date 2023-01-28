84787c0b51a4eada85873766096679caab3659ea
Ivan Arsenev <42762155+ancill@users.noreply.github.com>
2023-01-28 12:54:33 -0300

    1

---
+++ src/components/NavBottomBar.tsx
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+type Props = {};
+
+const routesConfig = {
+  home: "/home",
+  decks: "/decks",
+  leader: "/leader",
+  settings: "/settings",
+};
+
+const navBarConfig = [
+  {
+    title: "Home",
+    route: routesConfig.home,
+    icon: () => (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          stroke-width="2"
+          d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: "Decks",
+    route: routesConfig.decks,
+    icon: () => (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          stroke-width="2"
+          d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: "Leader Board",
+    route: routesConfig.leader,
+    icon: () => (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          stroke-width="2"
+          d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: "Settings",
+    route: routesConfig.settings,
+    icon: () => (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          stroke-width="2"
+          d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+        />
+      </svg>
+    ),
+  },
+];
+const NavBottomBar = (props: Props) => {
+  const [active, setActive] = useState();
+  const [activePage, setActivePage] = useState(routesConfig.home);
+
+  return (
+    <div className="btm-nav">
+      {navBarConfig.map((el) => (
+        <button
+          key={el.title}
+          className={`text-primary ${
+            activePage === el.route ? "active delay-200 ease-out" : ""
+          }`}
+          onClick={() => setActivePage(el.route)}
+        >
+          {el.title}
+          {el.icon()}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default NavBottomBar;
