7d0d8e6c23a8c39e4231c4797c7b1cd80a8b1d3f
Ivan Arsenev <42762155+ancill@users.noreply.github.com>
2023-02-03 13:48:06 -0300

    done #14  + docker postgress

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,18 +1,53 @@
+import { DeckCategory } from "@prisma/client";
 import { prisma } from "../src/server/db";
 
 async function main() {
-  const id = "cl9ebqhxk00003b600tymydho";
-  await prisma.example.upsert({
-    where: {
-      id,
+  // deckCategory seed
+  const collection = await prisma.deckCollection.create({
+    data: {
+      label: "Dev",
+      category: DeckCategory.PERSONAL,
+      description: "Development collection",
     },
-    create: {
-      id,
-    },
-    update: {},
+  });
+
+  await prisma.card.createMany({
+    data: [
+      {
+        front: "reunir (v)",
+        back: "to put togerther, to get together [also juntar]",
+        imgUrl:
+          "https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg",
+        deckCollectionId: collection.id,
+        audioUrl: "",
+      },
+      {
+        front: "nadie (pron)",
+        back: "nobody, anybody",
+        imgUrl:
+          "https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg",
+        deckCollectionId: collection.id,
+        audioUrl: "",
+      },
+      {
+        front: "asunto",
+        back: "matter, issue, affair",
+        imgUrl:
+          "https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg",
+        deckCollectionId: collection.id,
+        audioUrl: "",
+      },
+      {
+        front: "which",
+        back: "cuál",
+        imgUrl:
+          "https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg",
+        deckCollectionId: collection.id,
+        audioUrl: "",
+      },
+    ],
   });
 }
-
 main()
   .then(async () => {
     await prisma.$disconnect();
