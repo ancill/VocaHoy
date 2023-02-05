d8406b56be6550885f406a9b1dbcdfec6fb9a227
Ivan Arsenev <42762155+ancill@users.noreply.github.com>
2023-02-05 14:16:37 -0300

    [Session] Create session object, put it in db, CRUD on it Fixes #3

--- src/server/api/routers/deck.ts
+++ src/server/api/routers/deck.ts
@@ -1,24 +1,8 @@
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const deckRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
-
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
-  }),
-
   getDeckCollection: protectedProcedure.query(({ ctx }) => {
     return ctx.prisma.deckCollection.findMany();
   }),
