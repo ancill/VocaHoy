ee44602cafd3d953c89598bfa2c49d920bab1237
Ivan Arsenev <42762155+ancill@users.noreply.github.com>
2023-02-26 13:14:44 -0300

    1

--- src/pages/library/index.tsx
+++ src/pages/library/index.tsx
@@ -3,8 +3,35 @@ import { api } from "../../utils/api";
 import Loader from "../../components/Loader";
 import CardCollectionGrid from "../../components/Library/CardCollectionGrid";
 
-const CollectionTitle = ({ label }: { label: string }) => (
-  <h1 className="p-6 text-4xl font-bold">{label}</h1>
+const CollectionTitle = ({ label }: { label: string }) => {
+  const handleCreateCollection = () => {
+    console.log("create collection");
+  };
+  return (
+    <div className="flex py-4">
+      <h1 className="mr-6 pt-1 text-4xl font-bold">{label}</h1>
+      <div className="dropdown-hover dropdown">
+        <label tabIndex={0} className="btn">
+          EDIT
+        </label>
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu rounded-box w-52 bg-base-100 p-2 shadow"
+        >
+          <li>
+            <a onClick={handleCreateCollection}>Create collection</a>
+          </li>
+          <li>
+            <a onClick={handleCreateCollection}>Edit collection</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const SessionTitle = ({ label }: { label: string }) => (
+  <h1 className="mr-6 py-4 text-4xl font-bold">{label}</h1>
 );
 
 function LibraryPage() {
@@ -15,7 +42,7 @@ function LibraryPage() {
 
   return (
     <>
-      <CollectionTitle label="Current Sessions" />
+      <SessionTitle label="Current Sessions" />
       <SessionsCarousel />
       <CollectionTitle label="Development" />
       <CardCollectionGrid
